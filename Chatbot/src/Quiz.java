2b2bd7b4d7334cd3fdc38a84b729fd0b1be733bd
koksharov1998 <koksharov1998@ya.ru>
2018-10-03 19:15:43 +0500

    wip

--- Chatbot/src/Quiz.java
+++ Chatbot/src/Quiz.java
@@ -1,37 +1,35 @@
-import java.util.Random;
-
 public class Quiz {
 
-	private int currentQuestionID = -1;
-	private Pair[] quiz;
+  private int currentQuestionID = -1;
+  private Pair[] quiz;
 
-	public Quiz(String fileName) {
-		QuizReader qr = new QuizReader(fileName);
-		quiz = qr.readFromFile();
-	}
+  public Quiz(String fileName) {
+    QuizReader qr = new QuizReader(fileName);
+    quiz = qr.readFromFile();
+  }
 
-	public String getCurrentQuestion() {
-		return quiz[currentQuestionID].getFirst();
-	}
+  public String getCurrentQuestion() {
+    return quiz[currentQuestionID].getFirst();
+  }
 
-	public boolean moveNextQuestion() {
-		currentQuestionID++;
-		if (currentQuestionID < quiz.length) {
-			return true;
-		} else {
-			return false;
-		}
-	}
+  public boolean moveNextQuestion() {
+    currentQuestionID++;
+    if (currentQuestionID < quiz.length) {
+      return true;
+    } else {
+      return false;
+    }
+  }
 
-	public boolean checkAnswer(User user, String answer) {
-		if (answer.equals(quiz[currentQuestionID].getSecond().toLowerCase())) {
-			user.upScore();
-			user.pushQuestion(currentQuestionID);
-			System.out.println("It's right!");
-			return true;
-		} else {
-			System.out.println("It's wrong!");
-			return false;
-		}
-	}
+  public boolean checkAnswer(User user, String answer) {
+    if (answer.equals(quiz[currentQuestionID].getSecond().toLowerCase())) {
+      user.upScore();
+      user.pushQuestion(currentQuestionID);
+      System.out.println("It's right!");
+      return true;
+    } else {
+      System.out.println("It's wrong!");
+      return false;
+    }
+  }
 }
