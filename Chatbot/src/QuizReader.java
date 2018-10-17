666a132d2a92bcc7e5a81d005ecc02c2c9041bef
Shcherbina99 <33349318+Shcherbina99@users.noreply.github.com>
2018-10-17 09:42:03 +0500

    wip

--- Chatbot/src/QuizReader.java
+++ Chatbot/src/QuizReader.java
@@ -7,9 +7,52 @@ import java.util.Scanner;
 public class QuizReader {
 
   private String fileName;
+  private InputStream inputStream;
+
+  public QuizReader(InputStream inputStream)
+  {
+    this.inputStream = inputStream;
+  }
 
   public QuizReader(String fileName) {
     this.fileName = fileName;
+  }
+
+  public Pair[] readFromStream() {
+    List<Pair> text = new ArrayList<Pair>();
+    try {
+      Reader fr = new Reader(inputStream);
+      Scanner scan = new Scanner(fr);
+      String questionPrefix = "Question: ";
+      String answerPrefix = "Answer: ";
+      String newAnswer = null;
+      String newQuestion = null;
+      while (scan.hasNextLine()) {
+        String line = scan.nextLine();
+        if (line.startsWith(questionPrefix)) {
+          newQuestion = line.substring(questionPrefix.length());
+          continue;
+        } else if (line.startsWith(answerPrefix)) {
+          newAnswer = line.substring(answerPrefix.length());
+        } else {
+          throw new Exception("File format is wrong");
+        }
+        if (newAnswer == null || newQuestion == null) {
+          throw new Exception("File format is wrong");
+        } else {
+          text.add(new Pair(newQuestion, newAnswer));
+          newAnswer = null;
+          newQuestion = null;
+        }
+      }
+      scan.close();
+      fr.close();
+    } catch (IOException e) {
+      System.out.println(e.getMessage());
+    } catch (Exception e) {
+      System.out.println(e.getMessage());
+    }
+    return text.toArray(new Pair[text.size()]);
   }
 
   public Pair[] readFromFile() {
