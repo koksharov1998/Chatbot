419e0da744c5c1c4883638498b8b604dcd88b78e
EAShcherbina <ej99@mail.ru>
2018-10-22 22:36:22 +0500

    Added Client and Server

---
+++ Chatbot/src/client/Client.java
@@ -0,0 +1,60 @@
+package client;
+import java.io.BufferedReader;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
+public class Client {
+  public static void main(String[] args) throws InterruptedException {
+
+// запускаем подключение сокета по известным координатам и нициализируем приём сообщений с консоли клиента
+    try(Socket socket = new Socket("localhost", 3345);
+        BufferedReader br =new BufferedReader(new InputStreamReader(System.in));
+        DataOutputStream oos = new DataOutputStream(socket.getOutputStream());
+        DataInputStream ois = new DataInputStream(socket.getInputStream()) ) {
+
+      System.out.println("Client connected to socket.");
+
+// проверяем живой ли канал и работаем если живой
+      while(!socket.isOutputShutdown()){
+
+// ждём консоли клиента на предмет появления в ней данных
+        if(br.ready()){
+          System.out.println("Client start writing in channel...");
+          String clientCommand = br.readLine();
+
+// пишем данные с консоли в канал сокета для сервера
+          oos.writeUTF(clientCommand);
+          oos.flush();
+          System.out.println("Clien sent message " + clientCommand + " to server.");
+
+          if(clientCommand.equalsIgnoreCase("quit")){
+            System.out.println("Client kill connections");
+            if(ois.read() > -1)     {
+              System.out.println("reading...");
+              String in = ois.readUTF();
+              System.out.println(in);
+            }
+            break;
+          }
+
+          System.out.println("Client sent message & start waiting for data from server...");
+          if(ois.read() > 0)     {
+            System.out.println("reading...");
+            String in = ois.readUTF();
+            System.out.println(in);
+          }
+        }
+      }
+      System.out.println("Closing connections & channels on clentSide - DONE.");
+
+    } catch (UnknownHostException e) {
+      e.printStackTrace();
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+  }
+}
---
+++ Chatbot/src/server/Server.java
@@ -0,0 +1,54 @@
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Server extends Thread
+{
+  public static void main(String[] args) throws InterruptedException {
+    try (ServerSocket server= new ServerSocket(3345)){
+      Socket client = server.accept();
+      System.out.print("Connection accepted.");
+
+      // канал записи в сокет
+      DataOutputStream out = new DataOutputStream(client.getOutputStream());
+
+      // канал чтения из сокета
+      DataInputStream in = new DataInputStream(client.getInputStream());
+
+      while(!client.isClosed()){
+        System.out.println("Server reading from channel");
+        String entry = in.readUTF();
+
+        System.out.println("READ from client message - "+entry);
+        System.out.println("Server try writing to channel");
+
+// инициализация проверки условия продолжения работы с клиентом по этому сокету по кодовому слову       - quit
+        if(entry.equalsIgnoreCase("quit")){
+          System.out.println("Client initialize connections suicide ...");
+          out.writeUTF("Server reply - "+entry + " - OK");
+          out.flush();
+          break;
+        }
+
+        out.writeUTF("Server reply - "+entry + " - OK");
+        System.out.println("Server Wrote message to client.");
+
+// освобождаем буфер сетевых сообщений (по умолчанию сообщение не сразу отправляется в сеть, а сначала накапливается в специальном буфере сообщений, размер которого определяется конкретными настройками в системе, а метод  - flush() отправляет сообщение не дожидаясь наполнения буфера согласно настройкам системы
+        out.flush();
+      }
+
+
+      System.out.println("Client disconnected");
+      System.out.println("Closing connections & channels.");
+
+      in.close();
+      out.close();
+      client.close();
+      System.out.println("Closing connections & channels - DONE.");
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+  }
+}
