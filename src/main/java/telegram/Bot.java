1f67203cc3b7c98d51ade563fff4a9641f8f032e
koksharov1998 <koksharov1998@ya.ru>
2018-12-02 23:35:28 +0500

    Realized Quiz in Telegram with some mistakes

--- src/main/java/telegram/Bot.java
+++ src/main/java/telegram/Bot.java
@@ -1,5 +1,8 @@
 package telegram;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import org.telegram.telegrambots.bots.DefaultBotOptions;
@@ -11,15 +14,21 @@ import org.telegram.telegrambots.meta.api.objects.replykeyboard.ReplyKeyboardMar
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardButton;
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardRow;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import server.Quiz;
+import server.QuizReader;
+import server.User;
 import wiki.WikiApi;
 
 
 public class Bot extends TelegramLongPollingBot {
 
   private static final String help = "Command list:\n/help -- shows command list\n/startQuiz -- starts quiz\n/startwiki -- finds on Wikipedia";
+  private static final String helpQuiz = "Command list:\nhelp -- shows command list\nrepeat -- repeat last question\nresult -- shows your score\nquit -- finishes our dialog";
   private static String botName;
   private static String token;
   private static int status = 0;
+  private static Quiz quiz;
+  private static User user;
 
   public Bot(String botName, String token, DefaultBotOptions options) {
     super(options);
@@ -41,7 +50,7 @@ public class Bot extends TelegramLongPollingBot {
 
   private void handle(String chatId, Message input) {
     String s = input.getText().toLowerCase();
-    if (status == 1){
+    if (status == 1) {
       String w = WikiApi.getWikiInformation(s);
       sendMsg(chatId, w);
       sendMsg(chatId, "If you want to find something, you need to repeat a command /startWiki");
@@ -54,6 +63,16 @@ public class Bot extends TelegramLongPollingBot {
         break;
       case "/startquiz":
         status = 2;
+        File file = new File("quiz.txt");
+        FileInputStream fileInputStream = null;
+        try {
+          fileInputStream = new FileInputStream(file);
+          QuizReader quizReader = new QuizReader(fileInputStream);
+          quiz = new Quiz(quizReader);
+          fileInputStream.close();
+        } catch (IOException e) {
+          e.printStackTrace();
+        }
         sendMsg(chatId, "Hello, dear user! What is your name?");
         break;
       case "/startwiki":
@@ -61,7 +80,60 @@ public class Bot extends TelegramLongPollingBot {
         sendMsg(chatId, "What do you want to find on Wikipedia?");
         break;
       default:
+        if (status == 2) {
+          status = 3;
+          user = new User(s, 1);
+          sendMsg(chatId,
+              user.getName() + "!\nI'm java-chatbot. :)\nI can do some interesting things.");
+          sendMsg(chatId, helpQuiz);
+          sendMsg(chatId, "Now we can start quiz! Let's go!");
+          quiz.moveNextQuestion();
+          sendMsg(chatId, quiz.getCurrentQuestion());
+          break;
+        }
+        if (status == 3) {
+          try {
+            handle(s, user, quiz, chatId);
+          } catch (IOException e) {
+            e.printStackTrace();
+          }
+          break;
+        }
         sendMsg(chatId, "Я тебя не понимаю. Попробуй команду /help");
+    }
+  }
+
+  private void handle(String input, User user, Quiz quiz, String chatId) throws IOException {
+    switch (input) {
+      case "help":
+        sendMsg(chatId, helpQuiz);
+        break;
+      case "quit":
+        sendMsg(chatId, "Your score: " + user.getScore());
+        sendMsg(chatId, "Bye!");
+        sendMsg(chatId, "quit");
+        status = 0;
+        break;
+      case "result":
+        sendMsg(chatId, "Your score: " + user.getScore());
+        break;
+      case "repeat":
+        sendMsg(chatId, quiz.getCurrentQuestion());
+        break;
+      default:
+        if (quiz.checkAnswer(user, input)) {
+          sendMsg(chatId, "It's right!");
+        } else {
+          sendMsg(chatId, "It's wrong!");
+        }
+        if (!quiz.moveNextQuestion()) {
+          sendMsg(chatId, "Your score: " + user.getScore());
+          sendMsg(chatId, "Bye!");
+          sendMsg(chatId, "quit");
+          status = 0;
+        } else {
+          sendMsg(chatId, quiz.getCurrentQuestion());
+        }
     }
   }
 
