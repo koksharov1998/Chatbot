d3dae774d73274984a23b848ddedfbf0ec8e398b
Shcherbina99 <ej99@mail.ru>
2018-12-11 13:21:05 +0500

    Works with xml

--- src/main/java/wiki/WikiApi.java
+++ src/main/java/wiki/WikiApi.java
@@ -1,11 +1,16 @@
 package wiki;
 
 import java.io.BufferedReader;
+import java.io.ByteArrayInputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
-import java.util.List;
-import org.json.JSONArray;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
 
 public class WikiApi {
 
@@ -13,7 +18,7 @@ public class WikiApi {
 
     request = request.replace(" ", "_");
     String query = "https://en.wikipedia.org/w/api.php?action=opensearch&search=" + request
-        + "&limit=1&format=json&utf8=1";
+        + "&limit=1&format=xml";
     HttpURLConnection connection = null;
     try {
       connection = (HttpURLConnection) new URL(query).openConnection();
@@ -26,12 +31,13 @@ public class WikiApi {
         BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
         String line;
         while ((line = in.readLine()) != null) {
-          JSONArray array = new JSONArray(line);
-          List l = array.toList();
-          String name = "<b>" + l.get(1).toString().substring(1, l.get(1).toString().length()-1) + "</b>";
-          String inf = "<i>" + l.get(2).toString().substring(1, l.get(2).toString().length()-1) + "</i>";
-          String ref = "<a href=\"" + l.get(3).toString().substring(1, l.get(3).toString().length()-1) + "\"> Источник</a>";
-
+          DocumentBuilder documentBuilder = DocumentBuilderFactory.newInstance().newDocumentBuilder();
+          Document document = documentBuilder.parse(new ByteArrayInputStream(line.getBytes()));
+          NodeList nodeList = document.getElementsByTagName("Item");
+          Element element = (Element) nodeList.item(0);
+          String name = "<b>" + getTagValue("Text",element) + "</b>";
+          String inf = "<i>" + getTagValue("Description",element) + "</i>";
+          String ref = "<a href=\"" + getTagValue("Url",element) + "\"> Источник</a>";
           sm.append(name);
           sm.append("\n");
           sm.append(inf);
@@ -42,6 +48,7 @@ public class WikiApi {
         return sm.toString();
       }
     } catch (Throwable cause) {
+      cause.printStackTrace();
       return "We don't know, what is it.";
     } finally {
       if (connection != null) {
@@ -49,5 +56,11 @@ public class WikiApi {
       }
     }
     return "!";
+  }
+
+  private static String getTagValue(String tag, Element element) {
+    NodeList nodeList = element.getElementsByTagName(tag).item(0).getChildNodes();
+    Node node = (Node) nodeList.item(0);
+    return node.getNodeValue();
   }
 }
